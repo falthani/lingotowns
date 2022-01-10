7be173a71d122e55d73428a5245329e2b4d6fba4
Fatima Althani <timaalthani@gmail.com>
2022-01-10 12:19:56 +0000

    fixed swiper buttons. prev and next.

--- server/static/js/tutorial-bubble-swiper.js
+++ server/static/js/tutorial-bubble-swiper.js
@@ -1,19 +1,34 @@
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    effect: 'fade',
-    loop: false,
-    speed: 300,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: false,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  effect: 'fade',
+  loop: false,
+  speed: 300,
+  preventClicks: false,
+  preventClicksPropagation: false,
+  simulateTouch: false,
+
+pagination: {
+  el: '.swiper-pagination',
+    clickable: false,
       dynamicBullets: true
-    },
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+},
+// Navigation arrows
+navigation: {
+  nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
     }
   });
 
-  swiper.mousewheel.disable()
+  // if(swiper.activeIndex === 3) {
+  //   $('.swiper-button-prev').hide()
+  //   $('.swiper-button-next').hide()
+  // }
+
+swiper.mousewheel.disable()
+
+
+  // } else if (swiper.activeIndex === 1) {
+  //   $('.swiper-button-prev').hide()
+  //   $('.swiper-button-next').hide()
+  // }
